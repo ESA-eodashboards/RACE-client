b836540fa7c81889b2568fd2ab4453c3120d7613
Lubomir <lubomir.dolezal@eox.at>
2025-07-19 22:54:30 +0200

    feat: update eodash and restructure pages as in eodashboard

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,10 +1,12 @@
-// https://vitepress.dev/guide/custom-theme
+// import { defineConfig } from "vitepress";
+// import baseConfig from "@eox/pages-theme-eox/config";
 import { h } from "vue";
 import DefaultTheme from "vitepress/theme";
 
-/** @type {import('vitepress').Theme} */
+// export default defineConfig({
 export default {
   extends: DefaultTheme,
+  // extends: baseConfig("RACE"),
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
@@ -14,6 +16,17 @@ export default {
     if (!import.meta.env.SSR) {
       await import("./style.css");
       await import("@eox/storytelling");
+      await import("@eox/layout");
+      await import("@eox/itemfilter");
+      await import ("@eox/map");
+      await import ("@eox/chart");
+      await import ("@eox/drawtools");
+      await import ("@eox/jsonform");
+      await import ("@eox/stacinfo");
+      await import ("@eox/layercontrol");
+      await import ("color-legend-element");
+      await import ("@eox/timecontrol");
+      await import ("@eox/ui");
       await import("../../.eodash/dist/eo-dash");
       await import("../../.eodash/dist/eo-dash.css");
     }
