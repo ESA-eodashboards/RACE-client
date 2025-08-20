9d70b610dd81ccc5253d6987e5c5dd10cd283c6e
silvester-pari <silvester-pari@users.noreply.github.com>
2025-08-20 09:54:45 +0000

    Deploy preview for PR 12 🛫

---
+++ pr-preview/pr-12/assets/chunks/ssrBoot-Zgc_Ttvi.DBSK8g6E.js
@@ -0,0 +1,1 @@
+import{B as t,p as e,ar as s,S as n}from"./framework.XaTFbGuv.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
