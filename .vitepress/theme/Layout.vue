27291f381977f16ec616ce4ba8173b176a71b1db
silvester-pari <silvester.pari@eox.at>
2025-08-20 11:53:15 +0200

    fix: render custom navbar only on client

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -13,14 +13,16 @@ onMounted(() => {
 
 <template>
   <div v-if="frontmatter.layout === 'eodash' || frontmatter.layout === 'story'">
-    <esa-navbar
-      fixed
-      :brand-logo="theme.logo"
-      :brand-title="site.title"
-      .menuItems="theme.nav.map((n) => ({ title: n.text, href: n.link }))"
-      :enable-login="theme.login"
-      style="--header-transform: translateY(0); --header-background: #001923; display: block; height: var(--vp-nav-height);"
-    ></esa-navbar>
+    <ClientOnly>
+      <esa-navbar
+        fixed
+        :brand-logo="theme.logo"
+        :brand-title="site.title"
+        .menuItems="theme.nav.map((n) => ({ title: n.text, href: n.link }))"
+        :enable-login="theme.login"
+        style="--header-transform: translateY(0); --header-background: #001923; display: block; height: var(--vp-nav-height);"
+      ></esa-navbar>
+    </ClientOnly>
     <Content></Content>
   </div>
   <Layout v-else></Layout>
